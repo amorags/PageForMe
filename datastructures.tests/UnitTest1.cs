23527e9cfb707884c5345d7b8ae0d7060b00f1a7
amorags <alex.stengaard@gmail.com>
2025-06-12 19:08:23 +0200

    wtflol

---
+++ datastructures.tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using Microsoft.EntityFrameworkCore;
+using PageForMe.Services;
+using PageForMe.Data;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PageForMe.Tests
+{
+    public class DataGeneratorServiceTests
+    {
+        private ApplicationDbContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb_" + Guid.NewGuid()) // Unique DB per test
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+
+        [Fact]
+        public async Task GenerateEmployeesAsync_Adds_ExpectedNumberOfEmployees()
+        {
+            // Arrange
+            var context = GetInMemoryDbContext();
+            var service = new DataGeneratorService(context);
+
+            int expectedCount = 50;
+
+            // Act
+            await service.GenerateEmployeesAsync(expectedCount);
+
+            // Assert
+            var actualCount = await context.Employees.CountAsync();
+            Assert.Equal(expectedCount, actualCount);
+        }
+    }
+}
